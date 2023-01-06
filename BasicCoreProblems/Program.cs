57a5c1c58b75a4a23017750f5e1d13b922217dd5
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-06 20:57:16 +0530

    Welcome to Basic Core Program

---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicCoreProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+            Console.WriteLine("Select the Program\n1.Flip Coin");
+            Console.WriteLine("Choose option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    UseCases usecases = new UseCases();
+                    usecases.FlipCoin();
+                    break;
+            }
+        }
+    }
+}
---
+++ BasicCoreProblems/UseCases.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class UseCases
+    {
+        public void FlipCoin()
+        {
+            int tail = 0, head = 0;
+            Console.WriteLine("Enter number of times to flip");
+            int n = Convert.ToInt32(Console.ReadLine());           
+            for (int i = 0; i < n; i++)
+            {
+                Random random = new Random();
+                int coinFlipped = random.Next(2);
+                    if (coinFlipped < 0.5)
+                    {
+                        head++;
+                    }
+                    else
+                    {
+                        tail++;
+                    }                
+            }
+            Console.WriteLine("Head: " + head);
+            Console.WriteLine("Tail: " + tail);
+            int headPercentage = head * 100 / n;
+            Console.WriteLine("HeadsPercent = " + headPercentage + "%");
+            int tailPercentage = tail * 100 / n;
+            Console.WriteLine("TailsPercent = " + tailPercentage + "%");
+        }
+    }
+}
